86d78720a464772a9fcbcf24f3b66cbf3e884a65
josegutierrezmorals <josegutierrezmorals@gmail.com>
2026-05-14 18:49:10 -0400

    Update universe.sql

--- universe.sql
+++ universe.sql
@@ -1,10 +1,3 @@
---
--- PostgreSQL database dump
---
-
--- Dumped from database version 12.22 (Ubuntu 12.22-0ubuntu0.20.04.4)
--- Dumped by pg_dump version 12.22 (Ubuntu 12.22-0ubuntu0.20.04.4)
-
 SET statement_timeout = 0;
 SET lock_timeout = 0;
 SET idle_in_transaction_session_timeout = 0;
@@ -492,4 +485,3 @@ ALTER TABLE ONLY public.star
 --
 -- PostgreSQL database dump complete
 --
-
